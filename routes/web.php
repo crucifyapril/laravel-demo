5c078140941e912f65e413b8fe51e91136302a4a
hauntingclaire <danilpatuk@yandex.ru>
2024-05-12 20:07:47 +1000

    добавлены роуты

--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,11 @@
 use App\Http\Controllers\MainController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/demo', [MainController::class, 'index']);
+Route::get('/', [MainController::class, 'index']);
+Route::get('/reg', [MainController::class, 'reg']);
+Route::post('/reg', [MainController::class, 'regSuccess']);
+Route::get('/login', [MainController::class, 'login'])->name('login');
+Route::get('/logout', [MainController::class, 'logout']);
+Route::get('/about', [MainController::class, 'about']);
+Route::get('/news', [MainController::class, 'news'])->name('news');
+Route::post('/submit-news', [MainController::class, 'submitNews']);
