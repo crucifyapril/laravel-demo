abfa167abaa41ef864da37c2afb08831120a0796
hauntingclaire <danilpatuk@yandex.ru>
2024-05-13 21:24:46 +1000

    добавлены новые контроллеры

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,11 +2,36 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\News;
+use App\Models\User;
+use Exception;
+use Illuminate\Http\Request;
+
 class MainController
 {
     public function index()
     {
+        $latestNews = News::latest()->limit(10)->get();
 
-        return view('welcome', ['title' => 'XNJ _ NJ']);
+        return view('home', ['title' => 'Главная', 'news' => $latestNews]);
+    }
+
+    public function about()
+    {
+        return view('about', ['title' => 'О нас']);
+    }
+    public function news()
+    {
+        return view('news', ['title' => 'Добавить новость']);
+    }
+    public function submitNews(Request $request)
+    {
+        try {
+            News::query()->create($request->all());
+        } catch (Exception $e) {
+            return view('news', ['title' => 'Добавление новости', 'message' => 'Новость не добавлена']);
+        }
+
+        return view('news', ['title' => 'Добавление новости', 'message' => 'Новость добавлена']);
     }
 }
