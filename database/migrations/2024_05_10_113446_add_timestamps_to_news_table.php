9d0b6edf1b390d80f68d191c6ef73df6915d395d
hauntingclaire <danilpatuk@yandex.ru>
2024-05-13 21:30:40 +1000

    к миграции новостей добавлен timestamps

--- database/migrations/2024_05_10_113446_add_timestamps_to_news_table.php
+++ database/migrations/2024_05_10_113446_add_timestamps_to_news_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+$migration = new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -26,3 +25,4 @@ return new class extends Migration
         });
     }
 };
+return $migration;
