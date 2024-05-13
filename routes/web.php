e458a52faa6094103506d4a514eae85e6edc898e
hauntingclaire <danilpatuk@yandex.ru>
2024-05-13 21:13:26 +1000

    добавлены роуты по новому контроллеру

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,16 @@
 <?php
 
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, 'index']);
-Route::get('/reg', [MainController::class, 'reg']);
-Route::post('/reg', [MainController::class, 'regSuccess']);
-Route::get('/login', [MainController::class, 'login'])->name('login');
-Route::get('/logout', [MainController::class, 'logout']);
 Route::get('/about', [MainController::class, 'about']);
 Route::get('/news', [MainController::class, 'news'])->name('news');
 Route::post('/submit-news', [MainController::class, 'submitNews']);
+
+Route::get('/reg', [UserController::class, 'reg']);
+Route::post('/reg', [UserController::class, 'regSuccess']);
+Route::get('/login', [UserController::class, 'login'])->name('login');
+Route::post('/login', [UserController::class, 'loginSuccess']);
+Route::get('/logout', [UserController::class, 'logout']);
