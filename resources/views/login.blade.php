fa77157e8d0183d54150f4effcc28c3a2561e36a
hauntingclaire <danilpatuk@yandex.ru>
2024-05-13 21:19:25 +1000

    код стал меньше в шаблоне логин

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,22 +1,8 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/css/style.css">
-    <title>{{ $title }}</title>
-</head>
-<body>
+@extends('app')
 
-<div class="topnav">
-    <a href="/">Home</a>
-    <a href="/reg">Регистрация</a>
-    <a class="active" href="/login">Авторизация</a>
-    <a href="/news">Новости</a>
-    <a href="/about">О нас</a>
-</div>
+@section('content')
+<h1>{{ $message ?? '' }}</h1>
+<h1>{!! session()->get('error') !!}</h1>
 
 <form id="logh" action="/login" method="post">
     @csrf
@@ -31,7 +17,7 @@
 
         <label for="psw"><b>Пароль</b></label>
         <label>
-            <input type="password" placeholder="Введите пароль" name="psw" required>
+            <input type="password" placeholder="Введите пароль" name="password" required>
         </label>
         <hr>
 
@@ -43,5 +29,4 @@
         <p>У вас нет учетной записи? <a href="/reg">Зарегистрироваться.</a>.</p>
     </div>
 </form>
-</body>
-</html>
+@endsection
